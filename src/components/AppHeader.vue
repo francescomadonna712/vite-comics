5bdfbac7eac3410e887067ad0a826f615d790e2a
francesco madonna <fra.mad.93@hotmail.it>
2024-05-23 18:46:22 +0200

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,29 +1,103 @@
 <script>
+export default {
+    data(){
+      return{
+        
+        menuItems: [
 
+          {
+            itemClass: 'menu-item',
+            itemSpec: 'CHARACTER',
+          },
+          {
+            itemClass: 'menu-item',
+            itemSpec: 'COMICS',
+          },
+          {
+            itemClass: 'menu-item',
+            itemSpec: 'MOVIES',
+          },
+          {
+            itemClass: 'menu-item',
+            itemSpec: 'TV',
+          },
+          {
+            itemClass: 'menu-item',
+            itemSpec: 'GAMES',
+          },
+          {
+            itemClass: 'menu-item',
+            itemSpec: 'COLLECTIBLES',
+          },
+          {
+            itemClass: 'menu-item',
+            itemSpec: 'VIDEOS',
+          },
+          {
+            itemClass: 'menu-item',
+            itemSpec: 'FANS',
+          },
+          {
+            itemClass: 'menu-item',
+            itemSpec: 'NEWS',
+          },
+          {
+            itemClass: 'menu-item',
+            itemSpec: 'SHOP',
+          },
+
+        ]
+
+
+      }
+    }
+  }
 </script>
 
 <template>
   <header>
-    <div class="container-header">
+    <div class="container header">
         <div class="logo">
             <img src="../assets/dc-logo.png" alt="">
         </div>
-        <div class="menu">
-            <ul>
-            <li>characters</li>
-        </ul>
-        </div>
+        
+            <ul class="menu">
+            <li v-for="(el,i) in menuItems" class="menu-item">{{ menuItems[i].itemSpec }}</li>
+          </ul>
+       
       
     </div>
     </header>
 </template>
 
 <style scoped>
-.container-header{
-    padding: 0 10rem;
+.container{
+    padding: 0 15rem;
     display: flex;
     flex-direction: row;
-    background-color: rgb(207, 43, 43);
     height: 20vh;
 }
+    
+.header{
+    justify-content: space-between;
+}
+
+.menu{
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 15px;
+    list-style: none;
+    
+}
+
+.menu-item{
+    color: rgb(110, 109, 109);
+    font-weight: 600;
+}
+.logo{
+    margin: 0.5rem;
+}
+
 </style>
